ba2855708374ddc81b4be3855dcf0fd5c84fc585
leonofre <leonardodias.14.ld@gmail.com>
2016-09-20 16:37:45 -0300

    Added widgets folder

---
+++ widgets/class-monk-language-switcher.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       https://github.com/brenoalvs/monk
+ * @since      1.0.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Widgets
+ */
