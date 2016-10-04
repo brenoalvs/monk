8fecba2d5c49fa3d1c67cf9a9fb608a051679b9d
leonofre <leonardodias.14.ld@gmail.com>
2016-10-04 16:14:32 -0300

    Monk Language Switcher following the widgets standards

--- widgets/partials/public-monk-language-switcher.php
+++ widgets/partials/public-monk-language-switcher.php
@@ -20,6 +20,7 @@ if ( isset( $_GET['lang'] ) ) {
 	$select_value = 'en_EN';
 }
 
+$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'monk' );
 $flag = ! empty( $instance['flag'] ) ? true : false;
 $name = ! empty( $instance['name'] ) ? true : false;
 $lang = ! empty( $instance['lang-native'] ) ? true : false;
@@ -47,36 +48,38 @@ $languages_flag = array(
 	'pt_BR' => 'http://image.flaticon.com/icons/svg/206/206597.svg',
 );
 ?>
-
-<ul id="monk-selector">
-	<?php foreach ( $languages as $key => $value ) : ?>
-		<?php if ( strcmp( $key, $select_value ) == 0 ) : ?>
-			<li class="monk-active-lang">
-				<span class="monk-active-lang-name">
-					<?php if ( $flag ) : ?>
-						<img class="monk-selector-flag" src="<?php esc_attr_e( $languages_flag[$key] ) ?>" alt="">
-					<?php endif; ?>
-					<?php if ( $name ) : ?>
-						<span><?php esc_html_e( $value ); ?></span>
-					<?php else : ?>
-						<span class="screen-reader-text"><?php esc_html_e( $value ); ?></span>
-					<?php endif; ?>
-				</span>
-			</li>
-		<?php else : ?>
-			<li class="monk-lang">
-				<a class="monk-selector-link" href="?lang=<?php esc_attr_e( $key, 'monk' ); ?>">
-					<?php if ( $flag ) : ?>
-						<img class="monk-selector-flag" src="<?php esc_attr_e( $languages_flag[$key] ) ?>" alt="">
-					<?php endif; ?>
-					<?php if ( $name ) : ?>
-						<span><?php esc_html_e( $value ); ?></span>
-					<?php else : ?>
-						<span class="screen-reader-text"><?php esc_html_e( $value ); ?></span>
-					<?php endif; ?>
-				</a>
-			</li>
-		<?php endif; ?>
-	<?php endforeach; ?>
-	<span class="dashicons dashicons-arrow-down monk-selector-arrow"></span>
-</ul>
+<section id="<?php echo $this->id; ?>" class="widget <?php echo $this->id_base; ?>">
+	<h2 class="widget_title"><?php esc_html_e( $title ); ?></h2>
+	<ul id="monk-selector">
+		<?php foreach ( $languages as $key => $value ) : ?>
+			<?php if ( strcmp( $key, $select_value ) == 0 ) : ?>
+				<li class="monk-active-lang">
+					<span class="monk-active-lang-name">
+						<?php if ( $flag ) : ?>
+							<img class="monk-selector-flag" src="<?php esc_attr_e( $languages_flag[$key] ) ?>" alt="">
+						<?php endif; ?>
+						<?php if ( $name ) : ?>
+							<span><?php esc_html_e( $value ); ?></span>
+						<?php else : ?>
+							<span class="screen-reader-text"><?php esc_html_e( $value ); ?></span>
+						<?php endif; ?>
+					</span>
+				</li>
+			<?php else : ?>
+				<li class="monk-lang">
+					<a class="monk-selector-link" href="?lang=<?php esc_attr_e( $key, 'monk' ); ?>">
+						<?php if ( $flag ) : ?>
+							<img class="monk-selector-flag" src="<?php esc_attr_e( $languages_flag[$key] ) ?>" alt="">
+						<?php endif; ?>
+						<?php if ( $name ) : ?>
+							<span><?php esc_html_e( $value ); ?></span>
+						<?php else : ?>
+							<span class="screen-reader-text"><?php esc_html_e( $value ); ?></span>
+						<?php endif; ?>
+					</a>
+				</li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+		<span class="dashicons dashicons-arrow-down monk-selector-arrow"></span>
+	</ul>
+</section>
