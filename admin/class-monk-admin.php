c119551a72bd78d1b1616f9beb3c578d4a623586
Chandonn <alexandrecorreadeoliveira@gmail.com>
2016-10-11 10:53:04 -0300

    Added a function to check drafts among posts

--- admin/class-monk-admin.php
+++ admin/class-monk-admin.php
@@ -191,7 +191,7 @@ class Monk_Admin {
 	}
 
 	/**
-	 * Function to create the main language box for posts
+	 * Function to create the main language meta box for posts
 	 *
 	 * @since    1.0.0
 	*/
@@ -215,7 +215,7 @@ class Monk_Admin {
 	public function monk_post_meta_box_field_render( $post ) {
 		global $current_screen;
 		$active_languages      = get_option( 'monk_active_languages' );
-		$post_default_language = get_post_meta( $post->ID, '_monk_default_post_language', true );
+		$post_default_language = get_post_meta( $post->ID, '_monk_post_default_language', true );
 		$post_translations     = get_post_meta( $post->ID, '_monk_post_add_translation', true ) ? get_post_meta( $post->ID, '_monk_post_add_translation', true ) : array();
 		$available_languages   = array(
 			'da_DK' => __( 'Danish', 'monk' ),
@@ -230,48 +230,46 @@ class Monk_Admin {
 		);
 		wp_nonce_field( basename( __FILE__ ), 'monk_post_meta_box_nonce' );
 
-		if ( $current_screen->action == 'add' ) {
-		?>
+		if ( $current_screen->action == 'add' || $post_default_language == '' ) : ?>
 		<div>
 			<h4><?php _e( 'Default post language', 'monk' ); ?></h4>
 			<p>
-				<select name="monk_default_post_language">
-				<?php foreach ( $available_languages as $lang_code => $lang_name ) :
-					if ( in_array( $lang_code, $active_languages ) ) { ?>
-						<option value="<?php echo esc_attr( $lang_name ); ?>"<?php selected( $post_default_language, $lang_name ); ?>>
-							<?php echo esc_html( $lang_name ); ?>
-						</option>
-				<?php   }
-						endforeach; ?>
+				<select name="monk_post_default_language">
+				<?php
+					foreach ( $available_languages as $lang_code => $lang_name ) :
+						if ( in_array( $lang_code, $active_languages ) ) :
+				?>
+					<option value="<?php echo esc_attr( $lang_name ); ?>"<?php selected( $post_default_language, $lang_name ); ?>>
+						<?php echo esc_html( $lang_name ); ?>
+					</option>
+				<?php endif; endforeach; ?>
 				</select>
 			</p>
 		</div>
 
-		<?php
-		} else { ?>
+		<?php else : ?>
 		<div class="monk-post-meta-text">
-			<h2><?php echo $post_default_language; ?></h2>
-			<label for="monk-post-add-translation"><?php _e( 'Language', 'monk' ); ?></label>
-			<a class="edit-post-status hide-if-no-js" style="display: inline;"><span aria-hidden="true" class="monk-add-translation">Add+</span> <span class="screen-reader-text">Add new translation</span></a>
+			<h2><?php echo __( 'Post in ', 'monk' ) . $post_default_language; ?></h2>
+			<label for="monk-post-add-translation"><?php _e( 'Translations', 'monk' ); ?></label>
+			<a class="edit-post-status hide-if-no-js"><span aria-hidden="true" class="monk-add-translation">Add+</span> <span class="screen-reader-text">Add new translation</span></a>
 		</div>
 		<div class="monk-post-meta-add-translation">
 			<fieldset>
-				<?php foreach ( $available_languages as $lang_code => $lang_name ) :
+				<?php
+					foreach ( $available_languages as $lang_code => $lang_name ) :
 					$lang_id = sanitize_title( $lang_code );
-					if ( in_array( $lang_code, $active_languages ) && $post_default_language != $lang_name ) { ?>
+						if ( in_array( $lang_code, $active_languages ) && $post_default_language != $lang_name ) : ?>
 						<label for="<?php echo $lang_id; ?>">
 						<input type="checkbox" id="<?php echo $lang_id; ?>" name="monk_post_add_translation[]" value="<?php echo $lang_code; ?>" <?php checked( in_array( $lang_code, $post_translations) ); ?> /><?php echo $lang_name; ?>
 						</label>
 						<br />
-				<?php   }
-					endforeach; ?>
+				<?php endif; endforeach; ?>
 			</fieldset>		
 		</div>
-		<?php
-		}
+		<?php endif;
 	}
 
-	public function monk_save_post_default_language( $post_id ) {
+	public function monk_save_post_meta_box( $post_id ) {
 		if ( !isset( $_POST['monk_post_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['monk_post_meta_box_nonce'], basename( __FILE__ ) ) ) {
 			return;
 		}
@@ -284,7 +282,7 @@ class Monk_Admin {
 		if ( isset( $_REQUEST['monk_post_default_language'] ) ) {
 			update_post_meta(
 				$post_id,
-				'_monk_default_post_language',
+				'_monk_post_default_language',
 				sanitize_text_field( $_POST['monk_post_default_language'] )
 			);
 		}
