ff780b97f48c56e738513c56fd8b59f6c11ac056
leonofre <leonardodias.14.ld@gmail.com>
2016-10-26 18:23:11 -0200

    Modified monk languages filter

--- admin/partials/monk-language-column.php
+++ admin/partials/monk-language-column.php
@@ -19,18 +19,31 @@ $monk_satan_id    = get_post_meta( $post_ID, '_monk_post_translations_id', true 
 $monk_satan       = get_option( 'monk_post_translations_' . $monk_satan_id );
 $default_language = get_option( 'monk_default_language' );
 $base_url         = admin_url( 'post.php?action=edit' );
+$active_languages = get_option( 'monk_active_languages' );
 
 if ( $monk_satan ) :
+
+$post_url = add_query_arg( array(
+	'post' => $post_ID,
+), $base_url );
 ?>
-	<?php
-		foreach ( $monk_satan as $lang_code => $id ) :
-			$post_url = add_query_arg( array(
-				'post' => $id,
-			), $base_url );
-	?>
 	<a href="<?php echo esc_url( $post_url ); ?>">
-		<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $lang_code ); ?>"></span>
+		<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
 	</a>
+	<?php 
+	
+	foreach ( $active_languages as $lang ) :
+		foreach ( $monk_satan as $lang_code => $post_id ) :
+			if ( $lang === $lang_code && $lang_code !== $default_language ) :
+				$post_url = add_query_arg( array(
+					'post' => $post_id,
+				), $base_url );
+				?>
+				<a href="<?php echo esc_url( $post_url ); ?>">
+					<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $lang_code ); ?>"></span>
+				</a>
+			<?php endif; ?>
+		<?php endforeach; ?>
 	<?php endforeach; ?>
 <?php 
 	else: 
