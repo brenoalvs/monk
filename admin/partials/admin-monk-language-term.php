dc3095cfcaeec65419e5dfb4cdd107a4888191e6
leonofre <leonardodias.14.ld@gmail.com>
2016-11-07 19:14:36 -0200

    Added languages selector to add new term

--- admin/partials/admin-monk-language-term.php
+++ admin/partials/admin-monk-language-term.php
@@ -1,15 +1,28 @@
 <?php		
-	global $monk_languages;
-	$languages = get_option( 'monk_active_languages' );
-	$taxonomies = get_taxonomies();
-	?>
-	<div class="form-field term-language-wrap">
-		<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
-		<select class="postform" id="monk-language" name="monk-language">
-			<option value="-1"><?php _e( 'none', 'monk' ); ?></option>
-			<?php foreach ( $languages as $language ) : ?>
-				<option value="<?php echo $language; ?>" class=""><?php echo $monk_languages[$language]['name']; ?></option>
-			<?php endforeach; ?>
-		</select>
-	</div>
-	<?php
+	if ( ! isset( $_GET['monk_term_id'] ) ) {
+		?>
+		<div class="form-field term-language-wrap">
+			<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
+			<select class="postform" id="monk-language" name="monk-language">
+				<?php foreach ( $languages as $language ) : ?>
+					<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $default_language, $language ); ?>><?php echo esc_html( $monk_languages[$language]['name'] ); ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+		<?php
+	} else {
+		$translation_lang = $_GET['translation_lang'];
+		$monk_term_id     = $_GET['monk_term_id'];
+		?>
+		<div class="form-field term-language-wrap">
+			<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
+			<input type="hidden" name="monk_term_id" value="<?php echo esc_attr( $monk_term_id ); ?>">
+			<select class="postform" id="monk-language" name="monk-language">
+				<?php foreach ( $languages as $language ) : ?>
+					<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $translation_lang, $language ); ?>><?php echo esc_html( $monk_languages[$language]['name'] ); ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+		<?php
+	}
+	
