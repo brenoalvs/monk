1a06b90d55010783332a3be88084ee90c1f00de6
leonofre <leonardodias.14.ld@gmail.com>
2016-11-07 19:15:33 -0200

    Added esc_ functions

--- admin/partials/admin-monk-language-update-term.php
+++ admin/partials/admin-monk-language-update-term.php
@@ -1,23 +1,16 @@
-<?php
-	$monk_language = get_term_meta( $term->term_id, 'monk-language', true );
-	global $monk_languages;
-	$languages = get_option( 'monk_active_languages' );
-	?>
-	<table class="form-table">
-		<tbody>
-			<tr class="form-field term-language">
-				<th scope="row">
-					<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
-				</th>
-				<td>
-					<select class="postform" id="monk-language" name="monk-language">
-						<option value="-1"><?php _e( 'none', 'monk' ); ?></option>
-						<?php foreach ( $languages as $language ) : ?>
-							<option value="<?php echo $language; ?>" <?php selected( $monk_language, $language ); ?>><?php echo $monk_languages[$language]['name']; ?></option>
-						<?php endforeach; ?>
-					</select>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	<?php
+<table class="form-table">
+	<tbody>
+		<tr class="form-field term-language">
+			<th scope="row">
+				<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
+			</th>
+			<td>
+				<select class="postform" id="monk-language" name="monk-language">
+					<?php foreach ( $languages as $language ) : ?>
+						<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $monk_language, $language ); ?>><?php echo esc_html( $monk_languages[$language]['name'] ); ?></option>
+					<?php endforeach; ?>
+				</select>
+			</td>
+		</tr>
+	</tbody>
+</table>
