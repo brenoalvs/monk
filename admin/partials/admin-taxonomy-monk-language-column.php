b74263fecea4acfdd39b4be0151090c6489bc8bd
leonofre <leonardodias.14.ld@gmail.com>
2016-11-08 15:19:28 -0200

    Fixed bug in languages column

--- admin/partials/admin-taxonomy-monk-language-column.php
+++ admin/partials/admin-taxonomy-monk-language-column.php
@@ -5,20 +5,19 @@
 		}
 	}
 
-	if ( $monk_term_satan ) :
-
-		$translation_term_url = add_query_arg( array(
-			'tag_ID' => $monk_term_satan_id,
-		), $base_url );
-	?>
-		<a href="<?php echo esc_url( $translation_term_url ); ?>">
-			<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
-		</a>
-		<?php 
-		
+	if ( $monk_term_satan ) :		
 		foreach ( $languages as $language ) :
 			foreach ( $monk_term_satan as $translation_code => $translation_id ) :
-				if ( $language === $translation_code && $translation_code !== $default_language ) :
+				if ( $language === $translation_code && $translation_code === $default_language ) :
+					$translation_term_url = add_query_arg( array(
+						'tag_ID' => $monk_term_satan_id,
+					), $base_url );
+				?>
+					<a href="<?php echo esc_url( $translation_term_url ); ?>">
+						<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
+					</a>
+				<?php
+				elseif ( $language === $translation_code && $translation_code !== $default_language ) :
 					$translation_term_url = add_query_arg( array(
 						'tag_ID' => $translation_id,
 					), $base_url );
