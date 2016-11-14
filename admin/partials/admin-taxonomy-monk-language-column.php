b11e49138f82f33dae533d2b645f10ef4c7e1f48
leonofre <leonardodias.14.ld@gmail.com>
2016-11-14 17:03:34 -0200

    Added comments and verification if WPINC is not defined

--- admin/partials/admin-taxonomy-monk-language-column.php
+++ admin/partials/admin-taxonomy-monk-language-column.php
@@ -1,40 +1,55 @@
-<?php 
-	foreach ( $taxonomies as $taxonomy) {
-		if ( $_GET['taxonomy'] === $taxonomy ) {
-			$base_url = admin_url( 'term.php?taxonomy=' . $taxonomy );
-		}
-	}
+<?php
+/**
+ * Show flags in Languages column on taxonomies list.
+ *
+ * @link       https://github.com/brenoalvs/monk
+ * @since      1.0.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Admin/Partials
+ */
 
-	if ( $monk_term_satan ) :		
-		foreach ( $languages as $language ) :
-			foreach ( $monk_term_satan as $translation_code => $translation_id ) :
-				if ( $language === $translation_code && $translation_code === $default_language ) :
-					$translation_term_url = add_query_arg( array(
-						'tag_ID' => $monk_term_satan_id,
-					), $base_url );
-				?>
-					<a href="<?php echo esc_url( $translation_term_url ); ?>">
-						<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
-					</a>
-				<?php
-				elseif ( $language === $translation_code && $translation_code !== $default_language ) :
-					$translation_term_url = add_query_arg( array(
-						'tag_ID' => $translation_id,
-					), $base_url );
-					?>
-					<a href="<?php echo esc_url( $translation_term_url ); ?>">
-						<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $translation_code ); ?>"></span>
-					</a>
-				<?php endif; ?>
-			<?php endforeach; ?>
-		<?php endforeach; ?>
-	<?php 
-		else: 
-			$translation_term_url = add_query_arg( array(
-					'tag_ID' => $term_id,
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+foreach ( $taxonomies as $taxonomy) {
+	if ( $_GET['taxonomy'] === $taxonomy ) {
+		$base_url = admin_url( 'term.php?taxonomy=' . $taxonomy );
+	}
+}
+
+if ( $monk_term_satan ) :		
+	foreach ( $languages as $language ) :
+		foreach ( $monk_term_satan as $translation_code => $translation_id ) :
+			if ( $language === $translation_code && $translation_code === $default_language ) :
+				$translation_term_url = add_query_arg( array(
+					'tag_ID' => $monk_term_satan_id,
 				), $base_url );
-	?>
-		<a href="<?php echo esc_url( $translation_term_url ); ?>">
-			<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
-		</a>
-	<?php endif; ?>
+			?>
+				<a href="<?php echo esc_url( $translation_term_url ); ?>">
+					<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
+				</a>
+			<?php
+			elseif ( $language === $translation_code && $translation_code !== $default_language ) :
+				$translation_term_url = add_query_arg( array(
+					'tag_ID' => $translation_id,
+				), $base_url );
+				?>
+				<a href="<?php echo esc_url( $translation_term_url ); ?>">
+					<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $translation_code ); ?>"></span>
+				</a>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	<?php endforeach; ?>
+<?php 
+	else: 
+		$translation_term_url = add_query_arg( array(
+				'tag_ID' => $term_id,
+			), $base_url );
+?>
+	<a href="<?php echo esc_url( $translation_term_url ); ?>">
+		<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $default_language ); ?>"></span>
+	</a>
+<?php endif; ?>
