698789da2392e31f149bc355be6316333a32d201
leonofre <leonardodias.14.ld@gmail.com>
2016-11-15 12:07:50 -0200

    Improved code standards

--- admin/partials/admin-monk-language-term.php
+++ admin/partials/admin-monk-language-term.php
@@ -11,16 +11,16 @@
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
 
 if ( ! isset( $_GET['monk_term_id'] ) ) {
 	?>
 	<div class="form-field term-language-wrap">
-		<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
+		<label for="monk-language"><?php esc_html_e( 'Monk language', 'monk' ); ?></label>
 		<select class="postform" id="monk-language" name="monk-language">
 			<?php foreach ( $languages as $language ) : ?>
-				<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $default_language, $language ); ?>><?php echo esc_html( $monk_languages[$language]['name'] ); ?></option>
+				<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $default_language, $language ); ?>><?php echo esc_html( $monk_languages[ $language ]['name'] ); ?></option>
 			<?php endforeach; ?>
 		</select>
 	</div>
@@ -30,14 +30,13 @@ if ( ! isset( $_GET['monk_term_id'] ) ) {
 	$monk_term_id     = $_GET['monk_term_id'];
 	?>
 	<div class="form-field term-language-wrap">
-		<label for="monk-language"><?php _e( 'Monk language', 'monk' ); ?></label>
+		<label for="monk-language"><?php esc_html_e( 'Monk language', 'monk' ); ?></label>
 		<input type="hidden" name="monk_term_id" value="<?php echo esc_attr( $monk_term_id ); ?>">
 		<select class="postform" id="monk-language" name="monk-language">
 			<?php foreach ( $languages as $language ) : ?>
-				<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $translation_lang, $language ); ?>><?php echo esc_html( $monk_languages[$language]['name'] ); ?></option>
+				<option value="<?php echo esc_attr( $language ); ?>" <?php selected( $translation_lang, $language ); ?>><?php echo esc_html( $monk_languages[ $language ]['name'] ); ?></option>
 			<?php endforeach; ?>
 		</select>
 	</div>
 	<?php
 }
-	
