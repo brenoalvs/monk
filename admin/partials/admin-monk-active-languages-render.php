577087c56cb9b326bd26ff5b0ea56ed4ba61a4b0
leonofre <leonardodias.14.ld@gmail.com>
2016-11-18 18:37:29 -0200

    Fixed bug in first access of Monk settings page

--- admin/partials/admin-monk-active-languages-render.php
+++ admin/partials/admin-monk-active-languages-render.php
@@ -15,8 +15,9 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 global $monk_languages;
-$active_languages    = get_option( 'monk_active_languages' );
-$default_language    = get_option( 'monk_default_language' );
+
+$active_languages = get_option( 'monk_active_languages' );
+$default_language = get_option( 'monk_default_language' );
 ?>
 <fieldset>
 <?php
@@ -27,7 +28,13 @@ $default_language    = get_option( 'monk_default_language' );
 	 */
 foreach ( $monk_languages as $lang_code => $lang_names ) :
 	$id          = sanitize_title( $lang_code );
-	$is_checked  = $default_language === $lang_code || in_array( $lang_code, $active_languages, true ) ? true : false;
+
+	if ( $active_languages ) {
+		$is_checked = $default_language === $lang_code || in_array( $lang_code, $active_languages, true ) ? true : false;
+	} else {
+		$is_checked = false;
+	}
+
 	$is_default  = $default_language === $lang_code ? true : false;
 ?>
 <label for="<?php echo esc_attr( 'monk-' . $id ); ?>" <?php if ( $is_default ) : ?> class="option-disabled" <?php endif; ?>>
