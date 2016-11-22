df02eb69d742569bdbbd9ad11e3505933d91ebf2
leonofre <leonardodias.14.ld@gmail.com>
2016-11-22 17:00:44 -0200

    Changed the first language to current term language in column content on posts list

--- admin/partials/monk-language-column.php
+++ admin/partials/monk-language-column.php
@@ -13,22 +13,28 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+$post_url = add_query_arg( array(
+	'post' => $post_id,
+), $base_url );
 ?>
 	<span class="hide-if-no-js dashicons dashicons-edit"></span>
 	<div class="monk-hide monk-column-translations-arrow"></div>
 	<div class="hide-if-js monk-column-translations">
-<?php
-if ( $monk_satan ) :
-	foreach ( $active_languages as $lang ) :
+<?php if ( $monk_satan ) : ?>
+	<a class="monk-translation-link monk-language" href="<?php echo esc_url( $post_url ); ?>">
+		<span class="monk-language-name"><?php echo esc_html( $monk_languages[ $monk_language ]['name'] ); ?></span>
+		<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $monk_language ); ?>"></span>
+	</a>
+	<?php foreach ( $active_languages as $lang ) :
 		foreach ( $monk_satan as $lang_code => $post_id ) :
-			if ( $lang === $lang_code ) :
+			if ( $lang === $lang_code && $lang_code !== $monk_language ) :
 				$post_url = add_query_arg( array(
 					'post' => $post_id,
 				), $base_url );
 				?>
 				<a class="monk-translation-link <?php if ( $lang_code === $monk_language ) : ?>monk-language<?php endif; ?>" href="<?php echo esc_url( $post_url ); ?>">
 					<span class="monk-language-name"><?php echo esc_html( $monk_languages[ $lang ]['name'] ); ?></span>
-					<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $lang_code ); ?>"></span>
+					<span class="monk-selector-flag flag-icon <?php echo esc_attr( 'flag-icon-' . $lang ); ?>"></span>
 				</a>
 			<?php endif; ?>
 		<?php endforeach; ?>
