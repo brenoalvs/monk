2e3c1774197b1774c0e99cb0d1d42efec99ec1d2
Breno Alvs <breno.alvs@hotmail.com>
2016-11-22 16:05:11 -0200

    Created monk_is_language_code validate function

---
+++ includes/monk-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Validates a language code.
+ * Validate if a language code value is secure to use across the application.
+ *
+ * @since  1.0.0
+ *
+ * @param  string $language_code 	A language code value to validate
+ * @return boolean 					Language code validation
+ */
+function monk_is_language_code( $language_code ) {
+	global $monk_languages;
+	$languages_codes = array_keys( $monk_languages );
+	
+	return in_array( $language , $language_codes );
+}
