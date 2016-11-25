dc4d19d085e5c43db20d752e61ce65f6c04af54e
leonofre <leonardodias.14.ld@gmail.com>
2016-11-25 15:57:31 -0200

    Removed Show Language Names and Show Native Language Name options
    Changed show flags to hide flags

--- widgets/partials/admin-monk-language-switcher.php
+++ widgets/partials/admin-monk-language-switcher.php
@@ -16,8 +16,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Languages', 'monk' );
 $flag = isset( $instance['flag'] ) ? ( bool ) $instance['flag'] : false;
-$name = isset( $instance['name'] ) ? ( bool ) $instance['name'] : false;
-$lang = isset( $instance['lang-native'] ) ? ( bool ) $instance['lang-native'] : false;
 ?>
 <p class="monk-widgets-form">
 	<label for="monk-title"><?php esc_html_e( 'Title: ', 'monk' ); ?></label> 
@@ -25,16 +23,6 @@ $lang = isset( $instance['lang-native'] ) ? ( bool ) $instance['lang-native'] : 
 	
 	<label class="monk-label-block" for="<?php echo esc_attr( $this->get_field_id( 'flag' ) ); ?>">
 		<input class="monk-input-checkbox" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'flag' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'flag' ) ); ?>"<?php checked( $flag ); ?> />
-		<?php esc_html_e( 'Show Flags', 'monk' ); ?>
-	</label>
-
-	<label class="monk-label-block" for="<?php echo esc_attr( $this->get_field_id( 'name' ) ); ?>">
-		<input class="monk-input-checkbox" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'name' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'name' ) ); ?>" <?php checked( $name ); ?> />
-		<?php esc_html_e( 'Show Language Names', 'monk' ); ?>
-	</label>
-
-	<label class="monk-label-block" for="<?php echo esc_attr( $this->get_field_id( 'lang-native' ) ); ?>">
-		<input class="monk-input-checkbox" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'lang-native' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'lang-native' ) ); ?>" <?php checked( $lang ); ?> />
-		<?php esc_html_e( 'Show Native Language Name', 'monk' ); ?><small><?php esc_html_e( '(English is default)', 'monk' ); ?> </small>		
+		<?php esc_html_e( 'Hide Flags', 'monk' ); ?>
 	</label>
 </p>
