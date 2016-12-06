1c4db8cc72d53c7a4cdfe26979eb5f023d692aa6
Chandonn <alexandrecorreadeoliveira@gmail.com>
2016-12-06 18:20:28 -0200

    Added file that provides view for the admin notice

---
+++ admin/partials/admin-monk-notice-render.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Provide the view for the monk_display_activation_notice function
+ *
+ * @link       https://github.com/brenoalvs/monk
+ * @since      1.0.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Includes
+ */
+
+?>
+<div class="updated notice is-dismissible">
+	<p>
+		<h2><?php esc_html_e( 'Monk has been activated, go check the settings', 'monk' ); ?></h2>
+	</p>
+	<p>
+		<a href="<?php esc_attr_e( 'admin.php?page=monk', 'monk' ); ?>" class="button button-primary"><?php esc_html_e( 'Go to Monk settings', 'monk' ); ?></a>
+	</p>
+</div>
