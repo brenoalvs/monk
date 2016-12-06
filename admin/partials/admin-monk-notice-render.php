4020985b54c026a416cb4940730b05f1f82cfdb4
Chandonn <alexandrecorreadeoliveira@gmail.com>
2016-12-06 18:32:53 -0200

    Adjusted styles for the notice banner

--- admin/partials/admin-monk-notice-render.php
+++ admin/partials/admin-monk-notice-render.php
@@ -10,10 +10,8 @@
  */
 
 ?>
-<div class="updated notice is-dismissible">
-	<p>
+<div class="updated notice is-dismissible monk-banner">
 		<h2><?php esc_html_e( 'Monk has been activated, go check the settings', 'monk' ); ?></h2>
-	</p>
 	<p>
 		<a href="<?php esc_attr_e( 'admin.php?page=monk', 'monk' ); ?>" class="button button-primary"><?php esc_html_e( 'Go to Monk settings', 'monk' ); ?></a>
 	</p>
