7ae1b0e8847bc1198923368a82abb58d17753c61
Breno Alvs <breno.alvs@hotmail.com>
2016-12-10 01:49:04 -0200

    Added properly i18n on monk activation notice template

--- admin/partials/admin-monk-notice-render.php
+++ admin/partials/admin-monk-notice-render.php
@@ -11,8 +11,5 @@
 
 ?>
 <div class="updated notice is-dismissible monk-banner">
-		<h2><?php esc_html_e( 'Monk has been activated, go check the settings', 'monk' ); ?></h2>
-	<p>
-		<a href="<?php esc_attr_e( 'admin.php?page=monk', 'monk' ); ?>" class="button button-primary"><?php esc_html_e( 'Go to Monk settings', 'monk' ); ?></a>
-	</p>
+	<p><?php printf( __( '<strong>Monk:</strong> You need to <a href="%s">configure your language preferences</a>.' ), admin_url( 'admin.php?page=monk' ) ); ?></p>
 </div>
