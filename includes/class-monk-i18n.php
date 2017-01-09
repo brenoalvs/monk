8c7e726e5185de25a4e25e0adc9e1e864cc161d6
leonofre <leonardodias.14.ld@gmail.com>
2017-01-09 16:31:43 -0200

    Added function to set locale according to language switcher on public view

--- includes/class-monk-i18n.php
+++ includes/class-monk-i18n.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Define the internationalization functionality
  *
@@ -42,6 +41,24 @@ class Monk_i18n {
 
 	}
 
+	/**
+	 * Change locale accordingly Language Switcher.
+	 *
+	 * @param  string $lang Language of locale.
+	 * @return string $lang New language of locale.
+	 */
+	public function monk_define_locale( $lang ) {
+		if ( is_admin() ) {
+			return $lang;
+		}
 
+		if ( isset( $_GET['lang'] ) ) {
+			$lang = sanitize_text_field( wp_unslash( $_GET['lang'] ) );
+		} else {
+			$lang = get_option( 'monk_default_language', false );
+		}
+
+		return $lang;
+	}
 
 }
