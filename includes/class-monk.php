85a7ea0af10ece2dc704f079d8387ec2aa4af415
chandonn <alexandrecorreadeoliveira@gmail.com>
2017-01-17 15:40:50 -0200

    Added link filters for: term, dates and post types

--- includes/class-monk.php
+++ includes/class-monk.php
@@ -187,8 +187,12 @@ class Monk {
 		$this->loader->add_action( 'rewrite_rules_array', $plugin_admin, 'monk_create_rewrite_functions' );
 		$this->loader->add_filter( 'post_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
 		$this->loader->add_filter( 'page_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
-		$this->loader->add_filter( 'term_link', $plugin_admin, 'monk_add_language_term_permalink', 10, 2 );
+		$this->loader->add_filter( 'term_link', $plugin_admin, 'monk_add_language_term_permalink', 10, 3 );
 		$this->loader->add_filter( 'post_type_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
+		$this->loader->add_filter( 'post_type_archive_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
+		$this->loader->add_filter( 'day_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
+		$this->loader->add_filter( 'month_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
+		$this->loader->add_filter( 'year_link', $plugin_admin, 'monk_add_language_post_permalink', 10, 2 );
 		$this->loader->add_filter( 'update_option_monk_active_languages', $plugin_admin, 'monk_flush_on_update' );
 
 		// end rewrite.
