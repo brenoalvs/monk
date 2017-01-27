fd0b2150ad56965fd4f0bba6b7060dd6515ade24
chandonn <alexandrecorreadeoliveira@gmail.com>
2017-01-27 12:38:49 -0200

    Added filter to change author urls using the appropriate language

--- includes/class-monk.php
+++ includes/class-monk.php
@@ -240,6 +240,7 @@ class Monk {
 		$this->loader->add_filter( 'term_link', $plugin_links, 'monk_add_language_term_permalink', 10, 3 );
 		$this->loader->add_filter( 'year_link', $plugin_links, 'monk_add_language_date_permalink', 10, 2 );
 		$this->loader->add_filter( 'month_link', $plugin_links, 'monk_add_language_date_permalink', 10, 2 );
+		$this->loader->add_filter( 'author_link', $plugin_links, 'monk_add_language_author_permalink', 20 );
 		$this->loader->add_filter( 'post_type_link', $plugin_links, 'monk_add_language_post_permalink', 10, 2 );
 		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_need_canonical_redirect', 4 );
 		$this->loader->add_action( 'rewrite_rules_array', $plugin_links, 'monk_create_rewrite_functions' );
