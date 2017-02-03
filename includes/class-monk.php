09f2b3b041d53cf7791c912264f4601346590c9e
chandonn <alexandrecorreadeoliveira@gmail.com>
2017-02-03 17:29:29 -0200

    Added filter hook to change search links
    Changed function ordenation to increase readability

--- includes/class-monk.php
+++ includes/class-monk.php
@@ -237,14 +237,15 @@ class Monk {
 		$this->loader->add_filter( 'day_link', $plugin_links, 'monk_add_language_date_permalink', 20, 2 );
 		$this->loader->add_filter( 'post_link', $plugin_links, 'monk_add_language_post_permalink', 20, 2 );
 		$this->loader->add_filter( 'page_link', $plugin_links, 'monk_add_language_page_permalink', 20, 2 );
-		$this->loader->add_filter( 'term_link', $plugin_links, 'monk_add_language_term_permalink', 10, 3 );
+		$this->loader->add_filter( 'term_link', $plugin_links, 'monk_add_language_term_permalink', 20, 3 );
 		$this->loader->add_filter( 'year_link', $plugin_links, 'monk_add_language_date_permalink', 20, 2 );
 		$this->loader->add_filter( 'month_link', $plugin_links, 'monk_add_language_date_permalink', 20, 2 );
 		$this->loader->add_filter( 'author_link', $plugin_links, 'monk_add_language_author_permalink', 20, 2 );
-		$this->loader->add_filter( 'search_link', $plugin_links, 'monk_add_language_search_permalink', 10, 2 );
+		$this->loader->add_filter( 'search_link', $plugin_links, 'monk_add_language_search_permalink', 20 );
 		$this->loader->add_filter( 'post_type_link', $plugin_links, 'monk_add_language_post_permalink', 20, 2 );
-		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_need_canonical_redirect', 4 );
+		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_redirect_search', 10 );
 		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_redirect_home_url', 10, 2 );
+		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_need_canonical_redirect', 4 );
 		$this->loader->add_action( 'rewrite_rules_array', $plugin_links, 'monk_create_rewrite_functions' );
 		$this->loader->add_filter( 'post_type_archive_link', $plugin_links, 'monk_add_language_post_permalink', 20, 2 );
 		$this->loader->add_filter( 'update_option_monk_active_languages', $plugin_links, 'monk_flush_on_update' );
