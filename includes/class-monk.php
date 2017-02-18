b4439cf35fdccd8648b3b40cc467c5f3351c0097
chandonn <alexandrecorreadeoliveira@gmail.com>
2017-02-18 10:04:42 -0200

    Changed priority and arguments number for the monk_create_rewrite_functions method

--- includes/class-monk.php
+++ includes/class-monk.php
@@ -253,7 +253,7 @@ class Monk {
 		$this->loader->add_filter( 'post_type_archive_link', $plugin_links, 'monk_add_language_post_permalink', 20, 2 );
 		$this->loader->add_action( 'get_search_form', $plugin_links, 'monk_change_search_form', 50 );
 		$this->loader->add_action( 'template_redirect', $plugin_links, 'monk_canonical_redirection', 5 );
-		$this->loader->add_action( 'rewrite_rules_array', $plugin_links, 'monk_create_rewrite_functions' );
+		$this->loader->add_action( 'rewrite_rules_array', $plugin_links, 'monk_create_rewrite_functions', 10, 1 );
 		$this->loader->add_filter( 'update_option_monk_active_languages', $plugin_links, 'monk_flush_on_update' );
 	}
 
