683dbe960ea2cccbcabf7ef3b31c63ab694115fb
Breno Alvs <breno.alvs@hotmail.com>
2017-02-18 20:33:42 -0200

    Fixed admin posts filter
    Fixed admin terms filter

--- includes/class-monk.php
+++ includes/class-monk.php
@@ -197,11 +197,11 @@ class Monk {
 		$this->loader->add_action( 'save_post', $plugin_admin, 'monk_save_post_meta_box', 10, 2 );
 		$this->loader->add_action( 'wp_trash_post', $plugin_admin, 'monk_delete_post_data' );
 		$this->loader->add_action( 'before_delete_post', $plugin_admin, 'monk_delete_post_data' );
-		$this->loader->add_filter( 'get_terms_defaults', $plugin_admin, 'monk_category_language_filter', 10, 2 );
 		$this->loader->add_action( 'customize_register', $plugin_admin, 'monk_language_customizer' );
 		$this->loader->add_action( 'wp_head', $plugin_admin, 'monk_customize_css' );
 		$this->loader->add_action( 'restrict_manage_posts', $plugin_admin, 'monk_admin_languages_selector' );
-		// $this->loader->add_filter( 'pre_get_posts', $plugin_admin, 'monk_admin_languages_filter' );
+		$this->loader->add_filter( 'pre_get_posts', $plugin_admin, 'monk_admin_posts_filter' );
+		$this->loader->add_filter( 'get_terms_defaults', $plugin_admin, 'monk_admin_terms_filter' );
 		$this->loader->add_filter( 'manage_posts_columns', $plugin_admin, 'monk_language_column_head' );
 		$this->loader->add_filter( 'manage_pages_columns', $plugin_admin, 'monk_language_column_head' );
 		$this->loader->add_action( 'manage_posts_custom_column', $plugin_admin, 'monk_language_column_content', 10, 2 );
