7fd808337f4099e1b06d56a324c7d129ed170ccb
Leonofre <leonardodias.14.ld@gmail.com>
2017-02-18 22:08:39 -0200

    Changed from ids to class removed name and added class to select

--- admin/partials/admin-monk-language-selector-render.php
+++ admin/partials/admin-monk-language-selector-render.php
@@ -15,10 +15,10 @@ if ( ! defined( 'WPINC' ) ) {
 
 $attach = 'monk-attach';
 ?>
-<select name="monk_post_translation_id">
+<select name="monk_post_translation_id" class="monk-lang">
 <?php
 foreach ( $active_languages as $lang_code ) {
-	if ( array_key_exists( $lang_code, $monk_languages ) && $language_code !== $lang_code ) {
+	if ( array_key_exists( $lang_code, $monk_languages ) && $language_code !== $lang_code && ! array_key_exists( $lang_code, $post_translations ) ) {
 		if ( $lang_code === $language ) :
 			?>
 			<option value='<?php echo esc_attr( $lang_code ); ?>' selected="selected">
@@ -43,8 +43,8 @@ foreach ( $active_languages as $lang_code ) {
 ?>
 </select>
 
-<input type="hidden" name="monk_id" id="monk-id" value="<?php echo esc_attr( $monk_id ); ?>">
-<input type="hidden" id="previous-post-id" value="<?php echo esc_attr( $post_id ); ?>">
-<button class="button" id="<?php echo esc_attr( $attach ); ?>">
+<input type="hidden" name="monk_id" class="monk-id" value="<?php echo esc_attr( $monk_id ); ?>">
+<input type="hidden" class="previous-post-id" value="<?php echo esc_attr( $post_id ); ?>">
+<button class="button <?php echo esc_attr( $attach ); ?>">
 	<?php echo esc_html__( 'Ok', 'monk' ); ?>
 </button>
