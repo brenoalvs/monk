3876d1a9c807c1efa46d8a783fe6d6c44861feca
Breno Alvs <breno.alvs@hotmail.com>
2017-03-10 22:29:17 -0300

    Testing code to trigger Travis CI

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -15,6 +15,8 @@ class SampleTest extends WP_UnitTestCase {
 	 */
 	function test_sample() {
 		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$var = true;
+
+		$this->assertTrue( $var );
 	}
 }
