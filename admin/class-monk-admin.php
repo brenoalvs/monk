9c06029f63891e8f25f38ed6a9545333acbe536b
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-03-14 13:00:24 +0000

    Small fix in monk_add_menu_translation_fields function

--- admin/class-monk-admin.php
+++ admin/class-monk-admin.php
@@ -1190,13 +1190,14 @@ class Monk_Admin {
 		}
 
 		$active_languages    = get_option( 'monk_active_languages', false );
-		$new_translation_url = admin_url( 'nav-menus.php?action=edit&menu=0&monk_id=' . $monk_id );
 
 		if ( '0' === filter_input( INPUT_GET, 'menu' ) ) {
 			$monk_id = filter_input( INPUT_GET, 'monk_id' );
 			require_once plugin_dir_path( __FILE__ ) . '/partials/admin-monk-new-menu-fields-render.php';
 		} else {
 			$monk_id = get_term_meta( filter_input( INPUT_GET, 'menu' ), '_monk_menu_translations_id', true );
+			$new_translation_url = admin_url( 'nav-menus.php?action=edit&menu=0&monk_id=' . $monk_id );
+
 			require_once plugin_dir_path( __FILE__ ) . '/partials/admin-monk-menu-translation-fields-render.php';
 		}
 	}
