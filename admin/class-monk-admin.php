559b83d662a6e90ad1febc78e7b1ed32b5834d53
Leonofre <leonardodias.14.ld@gmail.com>
2017-04-18 19:07:51 -0300

    Changed logic to verify if monk_id exists

--- admin/class-monk-admin.php
+++ admin/class-monk-admin.php
@@ -1226,8 +1226,8 @@ class Monk_Admin {
 		$default_language = get_option( 'monk_default_language', false );
 
 		if ( '0' === filter_input( INPUT_GET, 'menu' ) ) {
-			if ( filter_input( INPUT_GET, 'monk_id' ) ) {
-				$monk_id           = filter_input( INPUT_GET, 'monk_id' );
+			$monk_id = filter_input( INPUT_GET, 'monk_id' );
+			if ( $monk_id ) {
 				$menu_translations = get_option( 'monk_menu_translations_' . $monk_id, array() );
 			}
 			require_once plugin_dir_path( __FILE__ ) . '/partials/admin-monk-new-menu-fields-render.php';
