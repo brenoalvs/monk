d03c0986bebae26392eba663a5371e3d2e04fa32
Leonofre <leonardodias.14.ld@gmail.com>
2017-04-25 15:59:08 -0300

    Added necessary fields to download language packages

--- admin/partials/admin-monk-settings-render.php
+++ admin/partials/admin-monk-settings-render.php
@@ -13,18 +13,20 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 ?>
+	<div class="notice notice-success is-dismissible monk-notice" id="monk-notice"></div>
 	<div class="wrap">
 		<h2>Monk</h2>
 		<?php
 			update_option( 'monk_settings_notice', false );
 			settings_errors();
 		?>
-		<form action="options.php" method="POST">
+		<form action="options.php" method="POST" id="monk-form-settings">
 			<?php
 				settings_fields( 'monk_settings' );
 				do_settings_sections( 'monk_settings' );
-				submit_button();
+				submit_button( '', 'primary', 'submit', 'true', array( 'id' => 'monk-submit-settings' ) );
 			?>
+			<span class="spinner monk-spinner" id="monk-spinner"></span>
 		</form>
 	</div>
 <?php
