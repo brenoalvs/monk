eac7a84d8c1911f4cb986ac64a7c6ff08d0273fc
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-04-27 02:49:49 -0300

    Added reference to the WP_UnitTest_Factory class

--- tests/test-save-post.php
+++ tests/test-save-post.php
@@ -1,33 +1,36 @@
 <?php
 require_once( '../../admin/class-monk-admin.php' );
+require_once( 'wptests/lib/factory.php' );
 
 class Save_Post_Test extends WP_UnitTestCase {
 
 	private $monk;
+	private $factory;
 
 	function setUp() {
 
 		parent::setUp();
-		$this->monk = new Monk_Admin( 'monk', '0.3.0' );
+		$this->monk    = new Monk_Admin( 'monk', '0.3.0' );
+		$this->factory = new WP_UnitTest_Factory;
 
 	} // end setup
 
 	function testSavePost() {
 
-		$post = array(
-			'ID' => 1,
-		);
+		// Use the factory to create a new post
+		$post_id = $this->factory->post->create();
 
+		// simulates the language from a form
 		$_POST['monk_post_language'] = 'en';
 
-		// monk_set_element_language( $type, $id, $language )
-		$this->monk->monk_set_element_language( 'post', 1, $_POST['monk_post_language'] );
+		// monk_set_post_language( $id, $language )
+		$this->monk->monk_set_post_language( $post_id, $_POST['monk_post_language'] );
 
-		// monk_get_element_language( $type, $id )
-		$language = $this->monk->monk_get_element_language( 'post', 1 );
+		// monk_get_post_language( $id )
+		$language = $this->monk->monk_get_element_language( $post_id );
 
-		$this->assertEquals( 'English', $language['name'] );
-		$this->assertEquals( 'en', $language['slug'] );
+		$this->assertEquals( 'en', $language );
+		$this->assertNotEmpty( $post_id );
 
 	} // end testSavePost
 
