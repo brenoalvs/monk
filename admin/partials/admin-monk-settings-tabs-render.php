b811dc773835e34fadc6036341fa99f218c84418
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-01 19:21:05 -0300

    Added tab render file

---
+++ admin/partials/admin-monk-settings-tabs-render.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Provide the view for the monk_setting_tabs function
+ *
+ * @since      0.4.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Admin/Partials
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
+	<h2 class="nav-tab-wrapper">
+	    <a href="<?php echo esc_url( $url . '&action=monk_general' ); ?>" class="nav-tab <?php echo esc_attr( $general ); ?>">General</a>
+	    <a href="<?php echo esc_url( $url . '&action=monk_tools' ); ?>" class="nav-tab <?php echo esc_attr( $tools ); ?>">Tools</a>
+	</h2>
