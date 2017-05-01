5eb8070062394d092a0e32a19957f2591e689ff3
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-01 19:23:18 -0300

    Added file to render checkbox related to set language to all posts and terms without language

---
+++ admin/partials/admin-monk-set-elements-language-render.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Provide the view for the monk_set_elements_language_render function
+ *
+ * @since      0.4.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Admin/Partials
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
+<input type="checkbox" name="monk_set_language_to_elements" id="monk_set_language_to_elements">
