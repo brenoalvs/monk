90f4e87f1a0fa6f1ea5639d3cebdd070d93a7f82
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-09 20:10:04 -0300

    Added new partial to display monk tools description

---
+++ admin/partials/admin-monk-tools-description.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Provide the view for the monk_options function
+ *
+ * @since      0.1.0
+ *
+ * @package    Monk
+ * @subpackage Monk/Admin/Partials
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
+<p><?php esc_html_e( 'Here you can option to set default language to all posts and terms without language.', 'monk' ); ?><br />
+<?php esc_html_e( 'Check the checkbox to confirm.', 'monk' ); ?></p>
+<?php
