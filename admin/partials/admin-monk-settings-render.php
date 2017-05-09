2b24d144b766549e4807ac66f69870c5d3748008
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-09 18:50:34 -0300

    Added div to display AJAX return message

--- admin/partials/admin-monk-settings-render.php
+++ admin/partials/admin-monk-settings-render.php
@@ -15,23 +15,30 @@ if ( ! defined( 'WPINC' ) ) {
 ?>
 	<div class="wrap">
 		<h2>Monk</h2>
+	    <div class="notice monk-hide" id="monk-settings-notice">
+	        <p></p>
+	    </div>
 		<?php
 			update_option( 'monk_settings_notice', false );
 			settings_errors();
+		if ( 'monk_general' === $action || '' === $action ) {
+			$form_id = 'monk-general-form';
+			$is_tools = false;
+		} elseif ( 'monk_tools' === $action ) {
+			$form_id  = 'monk-tools-form';
+			$is_tools = true;
+		}
 		?>
-		<form action="options.php" method="POST">
+		<form action="options.php" method="POST" class="monk-form-settings" id="<?php echo esc_attr( $form_id ); ?>">
 			<?php
-			settings_fields( 'monk_settings' );
-			do_settings_sections( 'monk_settings' );
-			if ( 'monk_general' === $action || '' === $action ) {
-				submit_button();
-			} elseif ( 'monk_tools' === $action ) {
-				$btn_args = array(
-					'id' => 'btn-monk-tools',
-				);
-				submit_button( '', 'primary', 'submit', 'true', $btn_args );
-				?><span class="spinner monk-spinner" id="monk-spinner"></span><?php
+			if ( ! $is_tools ) {
+				settings_fields( 'monk_settings' );
+			} else {
+				wp_nonce_field( '_monk_nonce' );
 			}
+			do_settings_sections( 'monk_settings' );
+			submit_button();
+			?><span class="spinner monk-spinner" id="monk-spinner"></span><?php
 			?>
 		</form>
 	</div>
