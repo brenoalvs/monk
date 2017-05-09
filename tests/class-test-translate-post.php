366038c31860b498dfffb4b2e869988bd7f7038f
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-05-09 09:00:19 -0300

    Exported the test_add_language_to_post method to its own class

--- tests/class-test-translate-post.php
+++ tests/class-test-translate-post.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The test class for the translate post processes
+ * The class to test the posts translation groups
  *
  * @package    Monk
  * @subpackage Monk/Post Translation Tests
@@ -8,7 +8,7 @@
  */
 
 /**
- * Tests the methods related to the post creation and translation
+ * Tests the methods related to the posts translation processes
  *
  * @since      0.4.0
  *
@@ -42,43 +42,6 @@ class Test_Translate_Post extends WP_UnitTestCase {
 		$this->factory = new WP_UnitTest_Factory;
 
 	} // end setUp
-
-	/**
-	 * Tests the creation process of a post with its language.
-	 *
-	 * @since    0.4.0
-	 *
-	 * @return void
-	 */
-	function test_add_language_to_post() {
-
-		// Use the factory to create a new post and then test it.
-		$post_object = new Monk_Post_Translation( $this->factory->post->create() );
-		$this->assertNotEmpty( $post_object );
-
-		// Set a language for this post.
-		$post_object->set_language( 'en_US' );
-
-		// get and test if the language was set correctly.
-		$language = $post_object->get_language();
-		$this->assertEquals( 'en_US', $language );
-
-		// inserts the monk_id into the post.
-		$post_object->set_translation_group_id( '' );
-
-		// tests if the monk_id is the object post_id, in this case.
-		$monk_id = $post_object->get_translation_group_id();
-		$this->assertEquals( $post_object->get_the_post_id(), $monk_id );
-
-		// saves the translations option.
-		$post_object->save_translation_group( $language );
-
-		// tests the translations option.
-		$option = $post_object->get_translation_group( $monk_id );
-		$this->assertArrayHasKey( $language, $option );
-		$this->assertContains( $monk_id, $option );
-
-	} // end test_add_language_to_post.
 
 	/**
 	 * Tests the single post translation process.
