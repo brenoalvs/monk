7029b52508995528c1b7c5becdb8641b24ad3a2f
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-16 17:25:06 -0300

    Fixed changes requested

--- admin/partials/admin-monk-default-language-url-render.php
+++ admin/partials/admin-monk-default-language-url-render.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Provide the view for the monk_default_language_render function
+ * Provide the view for the monk_default_language_url_render function
  *
- * @since      0.1.0
+ * @since      0.4.0
  *
  * @package    Monk
  * @subpackage Monk/Admin/Partials
@@ -12,6 +12,6 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-$default_language_url = get_option( 'monk_default_language_url', false ) ? 'checked=checked' : '';
+$default_language_url = get_option( 'monk_default_language_url', false ) ? true : false;
 ?>
-<input type="checkbox" name="monk_default_language_url" <?php echo esc_attr( $default_language_url ); ?>>
+<input type="checkbox" name="monk_default_language_url" <?php checked( $default_language_url, true ); ?>>
