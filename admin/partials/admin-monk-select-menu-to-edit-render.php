e63e1d7157e306d3663388c8a14132ed99c05d40
Leonofre <leonardodias.14.ld@gmail.com>
2017-05-23 16:32:15 -0300

    Fixed bug on menus select

--- admin/partials/admin-monk-select-menu-to-edit-render.php
+++ admin/partials/admin-monk-select-menu-to-edit-render.php
@@ -23,6 +23,8 @@ foreach ( $monk_ids as $monk_id ) : ?>
 	$monk_id_obj       = get_term( $monk_id );
 	if ( array_key_exists( $default_language, $monk_translations ) ) {
 		$monk_id_obj = get_term( $monk_translations[ $default_language ] );
+	} else {
+		$monk_id_obj = get_term( reset( $monk_translations ) );
 	}
 	$has_language = ! empty( get_term_meta( $monk_id_obj->term_id, '_monk_menu_language', true ) ) ? true : false;
 	?>
