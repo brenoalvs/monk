0d76e1d6f47233ccfd5465cebe46bdcb68df6ea8
chandonn <alexandrecorreadeoliveira@gmail.com>
2017-08-18 10:38:00 -0300

    Fixed code standards with `case` statement

--- admin/partials/admin-monk-settings-render.php
+++ admin/partials/admin-monk-settings-render.php
@@ -28,11 +28,11 @@ if ( ! defined( 'WPINC' ) ) {
 					<input type="hidden" name="action" value="monk_set_language_to_elements" />
 					<?php wp_nonce_field( '_monk_tools' ); ?>
 				<?php break; ?>
-				<?php case 'options': ?>
+			<?php case 'options': ?>
 					<input type="hidden" name="action" value="monk_save_options" />
 					<?php wp_nonce_field( '_monk_site_options' ); ?>
 				<?php break; ?>
-				<?php case 'general': ?>
+			<?php case 'general': ?>
 				<?php default: ?>
 					<?php
 						$btn_args = array(
@@ -55,10 +55,10 @@ if ( ! defined( 'WPINC' ) ) {
 					<p class="monk-message monk-hide" id="monk-checkbox-not-selected-message"><?php esc_html_e( 'Select the checkbox.', 'monk' ); ?></p>
 					<p class="monk-message monk-hide" id="monk-bulk-action"><?php esc_html_e( 'Defining language for posts and terms...', 'monk' ); ?></p>
 				<?php break; ?>
-				<?php case 'options': ?>
+			<?php case 'options': ?>
 					<p class="monk-message monk-hide" id="monk-save-options"><?php esc_html_e( 'Saving Options...', 'monk' ); ?></p>
 				<?php break; ?>
-				<?php case 'general': ?>
+			<?php case 'general': ?>
 				<?php default: ?>
 					<p class="monk-message monk-hide" id="monk-downloading"><?php esc_html_e( 'Downloading packages...', 'monk' ); ?></p>
 				<?php break; ?>
