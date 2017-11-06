bd138c2147ee8db0ce288c85768802eaba94cfa4
leonofre <leonardodias.14.ld@gmail.com>
2017-11-06 21:31:38 -0200

    Changed class monk instantiation to Monk()->method()

--- includes/monk-functions.php
+++ includes/monk-functions.php
@@ -176,13 +176,12 @@ function monk_get_available_languages() {
  */
 function monk_get_translations() {
 	$monk_languages = monk_get_available_languages();
-	$monk = new Monk();
 
 	$translation_data         = array();
 	$active_languages_slug    = array();
 	$current_language_slug    = '';
-	$active_languages         = $monk->get_active_languages();
-	$default_language         = $monk->get_default_language();
+	$active_languages         = Monk()->get_active_languages();
+	$default_language         = Monk()->get_default_language();
 	$has_default_language_url = get_option( 'monk_default_language_url', false );
 	$current_locale           = monk_get_locale_by_slug( get_query_var( 'lang' ) );
 	$default_slug             = $monk_languages[ $default_language ]['slug'];
@@ -350,8 +349,7 @@ function monk_get_translations() {
  */
 function monk_translated_post_id( $id, $lang = null ) {
 	if ( empty( $lang ) ) {
-		$monk = new Monk();
-		$default_language = $monk->get_default_language();
+		$default_language = Monk()->get_default_language();
 		$lang             = get_query_var( 'lang', $default_language );
 	}
 
