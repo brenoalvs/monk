b73d18b2fb53cead3edb213a189ed3741190adc0
Chandonn <alexandrecorreadeoliveira@gmail.com>
2017-11-27 09:35:13 -0200

    Added the 'add translation' section with new elements

--- admin/partials/admin-monk-post-meta-box-field-render.php
+++ admin/partials/admin-monk-post-meta-box-field-render.php
@@ -141,7 +141,81 @@ if ( ! defined( 'WPINC' ) ) {
 		<?php if ( count( $active_languages ) !== $translation_counter ) : ?>
 		<strong><?php esc_html_e( 'Add a translation', 'monk' ); ?></strong>
 		<span class="screen-reader-text"><?php esc_html_e( 'Add new translation', 'monk' ); ?></span>
+		<div>
+			<div class="monk-inner-section-elements">
+				<label for="monk-post-translation-id"><?php esc_html_e( 'Language', 'monk' ); ?></label>
+				<select name="monk_post_translation_id" class='monk-lang' id="monk-post-translation-id">
+					<?php
+					$post_type = get_post_type( $post->ID );
+					if ( 'post' !== $post_type && 'attachment' !== $post_type ) :
+						foreach ( $active_languages as $lang_code ) :
+							$language_url = add_query_arg( array(
+								'post_type' => $post_type,
+								'lang'      => $lang_code,
+								'monk_id'   => $monk_id,
+							), $monk_translation_url );
+							$lang_id      = sanitize_title( $lang_code );
+							if ( array_key_exists( $lang_code, $monk_languages ) && ! array_key_exists( $lang_code, $post_translations ) ) :
+								$lang_name = $monk_languages[ $lang_code ]['english_name'];
+						?>
+								<option value="<?php echo esc_url( $language_url ); ?>"/>
+									<?php echo esc_html( $lang_name ); ?>
+								</option>
+						<?php
+							endif;
+						endforeach;
+					elseif ( 'attachment' === $post_type ) :
+						$monk_translation_url = admin_url( 'media-new.php' );
+						foreach ( $active_languages as $lang_code ) :
+							$lang_id = sanitize_title( $lang_code );
+							if ( array_key_exists( $lang_code, $monk_languages ) && ! array_key_exists( $lang_code, $post_translations ) ) :
+								$lang_name = $monk_languages[ $lang_code ]['english_name'];
+						?>
+								<option value="<?php echo esc_attr( $lang_code ); ?>"/>
+									<?php echo esc_html( $lang_name ); ?>
+								</option>
+						<?php
+							endif;
+						endforeach;
+					else :
+						foreach ( $active_languages as $lang_code ) :
+							$language_url = add_query_arg( array(
+								'lang'    => $lang_code,
+								'monk_id' => $monk_id,
+							), $monk_translation_url );
+							$lang_id      = sanitize_title( $lang_code );
+							if ( array_key_exists( $lang_code, $monk_languages ) && ! array_key_exists( $lang_code, $post_translations ) ) :
+								$lang_name = $monk_languages[ $lang_code ]['english_name'];
+						?>
+								<option value="<?php echo esc_url( $language_url ); ?>"/>
+									<?php echo esc_html( $lang_name ); ?>
+								</option>
+						<?php
+							endif;
+						endforeach;
+					endif;
+					?>
+				</select>
+			<?php endif; ?>
+			<?php
+			$attach = ( 'attachment' === $post_type ) ? 'monk-attach' : '';
+			if ( $attach ) :
+			?>
+				<input type="hidden" name="monk_id" class="monk-id" value="<?php echo esc_attr( $monk_id ); ?>">
+				<input type="hidden" class="current-post-id" value="<?php echo esc_attr( $post->ID ); ?>">
+			</div>
+			<div id="monk-translation-actions">
+				<button class="button <?php echo esc_attr( $attach ); ?> button-primary button-large"><?php esc_html_e( 'Add', 'monk' ); ?></button>
+			</div>
+			<?php else : ?>
+				<label for="blank-translation" class="monk-form-label"><?php esc_html_e( 'Add new blank translation', 'monk' ); ?></label>
+				<input type="checkbox" name="monk_blank_translation" value="blank" id="blank-translation">
+			</div>
+			<div id="monk-translation-actions">
+				<button class="monk-submit-translation button button-primary button-large" id="<?php echo esc_attr( $attach ); ?>"><?php esc_html_e( 'Add', 'monk' ); ?></button>
+			</div>
+			<?php endif; ?>
+		</div>
 	</div>
-	<?php endif; ?>
 <?php
 endif;
